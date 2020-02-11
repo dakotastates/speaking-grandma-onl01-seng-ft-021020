7d070b6c060d7123b5cb32d14066d4a18169d8f7
Dakota States <dakota.states@gmail.com>
2020-02-11 04:16:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if not phrase.upcase 
+  if !phrase.upcase 
     return "HUH?! SPEAK UP, SONNY!"
   else
     if phrase == "I LOVE YOU GRANDMA!"
