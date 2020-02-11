aa618076602537c9617b60b4a2c5eb3253de17c0
Dakota States <dakota.states@gmail.com>
2020-02-11 04:00:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,10 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
-  if phrase.upcase
+def speak_to_grandma(eq)
+  if eq.upcase
     puts "NO, NOT SINCE 1939!"
-    elsif phrase == "I LOVE YOU GRANDMA!"
+    elsif eq == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
