f4db3d8ce04c44a334b5f6e92ffc64f0c6118312
Dakota States <dakota.states@gmail.com>
2020-02-11 04:13:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,9 +18,7 @@ def speak_to_grandma(phrase)
   else
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-  else
+    else
       return "NO, NOT SINCE 1939!"
-  else
-    
+    end
   end
-end
